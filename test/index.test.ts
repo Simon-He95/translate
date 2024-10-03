20ca528d7d0b4bed18baad0d29b5a2108181704f
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-10-03 23:13:45 +0800

    chore: update

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,7 +3,10 @@ import translateLoader from '../src'
 
 describe('should', () => {
   const translate = translateLoader()
-  it('exported', async () => {
+  it('zh->en', async () => {
     expect((await translate('你好'))[0]).toEqual('Hello')
+  })
+  it('en->zh', async () => {
+    expect((await translate('hello'))[0]).toEqual('你好')
   })
 })
